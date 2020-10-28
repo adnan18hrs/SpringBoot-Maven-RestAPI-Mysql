f0e44c690c33b99b1190ee3b5a1768a51b452170
adnan18hrs <Khan.adnan346@gmail.com>
2020-10-28 16:52:49 +0530

    ..

--- SmallWebApp/src/main/java/com/example/SmallWebApp/RegistrationController.java
+++ SmallWebApp/src/main/java/com/example/SmallWebApp/RegistrationController.java
@@ -6,10 +6,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class RegistrationController {
-	
 	@Autowired
 	UserRepo repo;
 	
